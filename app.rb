be0befe3cabd58b4da15a7b7967b16a9d1517ca6
Kameron Kenny <kkenny379@gmail.com>
2022-03-21 22:53:16 -0400

    add teams and players pages

--- app.rb
+++ app.rb
@@ -2,9 +2,17 @@ require 'rubygems'
 require 'sinatra'
 require 'pg'
 require 'uuidtools'
+require 'yaml'
+#require './config.rb'
 
 set :bind, '0.0.0.0'
 set :port, 9999
+
+class Dbc
+  def self.config
+    YAML.safe_load(ERB.new(File.read('db_config.yml')).result)
+  end
+end
 
 get '/' do
   f = File.open('score_left.txt', 'r')
@@ -67,6 +75,183 @@ get '/initialize' do
 end
 
 # TEAM ROUTES
+
+get '/teams' do
+  t = []
+  t_error = []
+
+  begin
+    db = PG.connect :dbname => Dbc.config['db']['name'],
+		    :user => Dbc.config['db']['user'],
+		    :password => Dbc.config['db']['pass'],
+		    :host => Dbc.config['db']['host']
+
+    teams = db.exec 'SELECT * FROM teams'
+
+    unless teams.nil?
+    puts teams
+      teams.each do |team|
+        t.unshift({
+	  team_id: team['team_id'],
+	  team_name: team['team_name'],
+	  team_year: team['team_year']
+	})
+      end
+    else
+      t.unshift({
+	team_id: ':(',
+	team_name: 'No Teams, ',
+	team_year: 'Yet. '
+      })
+    end
+
+  rescue PG::Error => e
+    t_error.unshift(e.message.to_s)
+
+  ensure
+    db.close if db
+
+  end
+
+  erb :teams, :locals => {:t => t, :t_error => t_error }
+end
+
+get '/teams/new' do
+  erb :teams_new
+end
+
+post '/teams/new/submit' do
+  if ( params[:team_name] != '' )
+    begin
+      db = PG.connect :dbname => Dbc.config['db']['name'],
+		      :user => Dbc.config['db']['user'],
+		      :password => Dbc.config['db']['pass'],
+		      :host => Dbc.config['db']['host']
+
+      team_uuid = UUIDTools::UUID.random_create.to_s
+
+      db.exec "INSERT INTO teams(team_id, team_name, team_year) \
+	       VALUES ('#{team_uuid}', '#{params[:team_name]}', '#{params[:team_year]}');"
+
+    rescue PG::Error => e
+      puts e.message.to_s
+
+    ensure
+      db.close if db
+
+    end
+  else
+    puts "team name should not be empty."
+  end
+
+  redirect "/teams"
+end
+
+
+# PLAYER ROUTES
+
+get '/players' do
+  p = []
+  p_error = []
+
+  begin
+    db = PG.connect :dbname => Dbc.config['db']['name'],
+		    :user => Dbc.config['db']['user'],
+		    :password => Dbc.config['db']['pass'],
+		    :host => Dbc.config['db']['host']
+
+    players = db.exec 'SELECT * FROM players'
+
+    players.each do |player|
+      t = []
+      team = db.exec "SELECT team_name FROM teams WHERE team_id='#{player['player_team_id']}'"
+
+      team.each do |t|
+	team_name = t['team_name']
+	break #Only set first team name if more exist.
+      end
+
+      p.unshift({
+	player_id: player['player_id'],
+	player_fname: player['player_fname'],
+	player_lname: player['player_lname'],
+	player_number: player['player_number'],
+	player_year: player['player_year'],
+	player_team: "#{team_name}"
+      })
+    end
+
+  rescue PG::Error => e
+    p_error.unshift(e.message.to_s)
+
+  ensure
+    db.close if db
+
+  end
+
+  erb :players, :locals => {:p => p, :p_error => p_error }
+end
+
+get '/players/new' do
+  t = []
+  t_error = []
+
+  begin
+    db = PG.connect :dbname => Dbc.config['db']['name'],
+		    :user => Dbc.config['db']['user'],
+		    :password => Dbc.config['db']['pass'],
+		    :host => Dbc.config['db']['host']
+
+    teams = db.exec 'SELECT * FROM teams'
+
+    teams.each do |team|
+      t.unshift({
+	team_id: team['team_id'],
+	team_name: team['team_name']
+      })
+    end
+
+  rescue PG::Error => e
+    t_error.unshift(e.message.to_s)
+
+  ensure
+    db.close if db
+
+  end
+
+  erb :players_new, :locals => {:t => t, :t_error => t_error }
+end
+
+post '/players/new/submit' do
+  player_school_id = "0"
+
+  if (( params[:team] != '' ) and
+      ( params[:player_number] != '' ))
+
+    begin
+      db = PG.connect :dbname => Dbc.config['db']['name'],
+		      :user => Dbc.config['db']['user'],
+		      :password => Dbc.config['db']['pass'],
+		      :host => Dbc.config['db']['host']
+
+      player_uuid = UUIDTools::UUID.random_create.to_s
+
+      db.exec "INSERT INTO players(player_id, player_fname, player_lname, player_number, player_team_id, player_school_id, player_year) \
+	       VALUES ('#{player_uuid}', '#{params[:player_fname]}', '#{params[:player_lname]}', '#{params[:player_number]}', '#{params[:team]}', '#{player_school_id}', '#{params[:player_year]}');"
+
+    rescue PG::Error => e
+      puts e.message.to_s
+
+    ensure
+      db.close if db
+
+    end
+  else
+    puts "team name and player number should not be empty."
+  end
+
+  redirect "/players"
+end
 
 
 # SCORE ROUTES
